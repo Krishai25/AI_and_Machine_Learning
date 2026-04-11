7f2a9edea92ff1ed76bfd3f6a689d0a130860e2c
V S Krishai <krishaiofficial25@gmail.com>
2026-04-11 12:49:39 +0530

    Data type and Variable Concept

---
+++ Python Basics/DatatypeandVar.py
@@ -0,0 +1,23 @@
+age=25
+print(age,type(age))
+
+percentage=88.76
+print(percentage,type(percentage))
+
+name="Krishai"
+print(name,type(name))
+
+is_active=True
+print(is_active,type(is_active))
+
+address=None
+print(address,type(address))
+
+# OverRiding of the Data Type
+
+x=10
+x="Hello World"
+x=99.99
+print(x,type(x))
+
+#prints the type as float
